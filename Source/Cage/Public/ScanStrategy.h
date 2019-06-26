47bff4eaac7b0682398b7b6dfd8a103693044fe1
Tomoaki Yoshida [ZEN] <yoshida@furo.org>
2019-06-26 11:37:30 +0900

    Workaround for UHT bug

--- Source/Cage/Public/ScanStrategy.h
+++ Source/Cage/Public/ScanStrategy.h
@@ -1,7 +1,8 @@
 ﻿// Copyright 2018 Tomoaki Yoshida<yoshida@furo.org>
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 #pragma once
 // Scan Strategy Interface
